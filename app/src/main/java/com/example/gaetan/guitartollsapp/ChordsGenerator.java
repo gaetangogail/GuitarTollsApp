d8187c2bcd999da13a90537a80e897321be5e052
gaetan <gogail.gaetan1@gmail.com>
2017-07-02 16:12:20 +0200

    ChordsGenerator modofication

--- app/src/main/java/com/example/gaetan/guitartollsapp/ChordsGenerator.java
+++ app/src/main/java/com/example/gaetan/guitartollsapp/ChordsGenerator.java
@@ -23,6 +23,8 @@ public class ChordsGenerator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chords_generator);
 
+
+        //Ce que je souhaiterais utiliser dans la classe CustomOnItemSelectedListener.java
         final ImageView imageView = (ImageView) findViewById(R.id.chords_image);
 
         button_selected = (Button) findViewById(R.id.button_selected);
@@ -32,92 +34,11 @@ public class ChordsGenerator extends AppCompatActivity {
         adapter_chords.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_chords.setAdapter(adapter_chords);
 
-        spinner_chords.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                switch(position) {
-                    default:
-                        Toast.makeText(getApplicationContext(), "vous choisissez C", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.c);
-                        break; // optional
-
-                    case 1 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez Db", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.db);
-                        break; // optional
-
-                    case 2 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez D", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.d);
-                        break;
-
-                    case 3 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez Eb", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.eb);
-                        break;
-
-                    case 4 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez E", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.e);
-                        break;
-
-                    case 5 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez F", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.f);
-                        break;
-
-                    case 6 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez Gb", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.gb);
-                        break;
-
-                    case 7 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez G", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.g);
-                        break; // optional
-
-                    case 8 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez Ab", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.ab);
-                        break;
-
-                    case 9 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez A", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.a);
-                        break;
-
-                    case 10 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez Bb", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.bb);
-                        break;
-
-                    case 11 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez B", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.b);
-                        break;
-
-                    case 12 :
-                        Toast.makeText(getApplicationContext(), "vous choisissez B", Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.b);
-                        break;
-
-
-                }
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+        // Spinner item selection Listener
+        //A voir pour utiliser avec un bouton
+        addListenerOnSpinnerItemSelection();
 
     }
-
-    // Spinner item selection Listener
-    //   addListenerOnSpinnerItemSelection();
 
 
     // Add spinner data
--- app/src/main/java/com/example/gaetan/guitartollsapp/CustomOnItemSelectedListener.java
+++ app/src/main/java/com/example/gaetan/guitartollsapp/CustomOnItemSelectedListener.java
@@ -16,33 +16,69 @@ public class CustomOnItemSelectedListener implements OnItemSelectedListener {
 
         switch(pos) {
             default:
-                Toast.makeText(parent.getContext(), "vous choisissez C", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez C", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.c);
                 break; // optional
 
             case 1 :
-                Toast.makeText(parent.getContext(), "vous choisissez D", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez Db", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.db);
                 break; // optional
 
             case 2 :
-                Toast.makeText(parent.getContext(), "vous choisissez E", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez D", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.d);
                 break;
 
             case 3 :
-                Toast.makeText(parent.getContext(), "vous choisissez F", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez Eb", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.eb);
                 break;
 
             case 4 :
-                Toast.makeText(parent.getContext(), "vous choisissez G", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez E", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.e);
                 break;
 
             case 5 :
-                Toast.makeText(parent.getContext(), "vous choisissez A", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez F", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.f);
                 break;
 
             case 6 :
-                Toast.makeText(parent.getContext(), "vous choisissez B", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "vous choisissez Gb", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.gb);
                 break;
 
+            case 7 :
+                Toast.makeText(getApplicationContext(), "vous choisissez G", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.g);
+                break; // optional
+
+            case 8 :
+                Toast.makeText(getApplicationContext(), "vous choisissez Ab", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.ab);
+                break;
+
+            case 9 :
+                Toast.makeText(getApplicationContext(), "vous choisissez A", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.a);
+                break;
+
+            case 10 :
+                Toast.makeText(getApplicationContext(), "vous choisissez Bb", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.bb);
+                break;
+
+            case 11 :
+                Toast.makeText(getApplicationContext(), "vous choisissez B", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.b);
+                break;
+
+            case 12 :
+                Toast.makeText(getApplicationContext(), "vous choisissez B", Toast.LENGTH_LONG).show();
+                imageView.setImageResource(R.drawable.b);
+                break;
 
         }
     }
